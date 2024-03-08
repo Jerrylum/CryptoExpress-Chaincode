68eb0b752f7acac1e2844e9190d383c33fdfa186
Jerrylum <serverofjerry@gmail.com>
2024-03-09 04:22:25 +0800

    Add logger and SerializerFullSchema  type definition to fabric-shim

--- src/types/fabric-shim-internal.d.ts
+++ src/types/fabric-shim-internal.d.ts
@@ -1,5 +1,6 @@
 import { Contract } from "fabric-contract-api";
 import { ChaincodeResponse, ChaincodeStub } from "fabric-shim";
+import winston = require("winston");
 
 declare class ChaincodeFromContract {
   constructor(
@@ -30,13 +31,27 @@ declare class ChaincodeFromContract {
   _splitFunctionName(fcn: string): { contractName: string; function: string };
 }
 
-interface Serializer {
-  toBuffer(result: any, schema?: {}, loggerPrefix?: string): Buffer;
+interface SerializerFullSchema {
+  properties: {
+    prop: NonNullable<{}>;
+  };
+  components: {
+    schemas: { [key: string]: NonNullable<{}> };
+  };
+}
 
-  fromBuffer(buffer: Buffer, schema: {}, loggerPrefix?: string): any;
+interface Serializer {
+  toBuffer(result: any, schema?: {}, loggerPrefix?: string): Buffer | undefined;
+
+  fromBuffer(data: {}, fullSchema: SerializerFullSchema, loggerPrefix?: string): { value: any; validateData: any };
 }
 
 interface Serializers {
   transaction: string;
   serializers: { [key: string]: new () => Serializer };
+}
+
+declare namespace logger {
+  function getLogger(name?: string): winston.Logger;
+  function setLevel(level: string): void;
 }
