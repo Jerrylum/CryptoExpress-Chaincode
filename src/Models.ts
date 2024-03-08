e20fc4a71fb6d19f3b4bca4208a6aa2a468b7b51
Jerrylum <serverofjerry@gmail.com>
2024-03-08 21:33:33 +0800

    Add classes to Models.ts

--- src/Models.ts
+++ src/Models.ts
@@ -1,72 +1,92 @@
 export type BufferObject = { type: "Buffer"; data: Buffer };
 
-export interface Address {
-  hashId: string;
-  line1: string;
-  line2: string;
-  recipient: string;
-  publicKey: BufferObject;
+export class Address {
+  hashId!: string;
+  line1!: string;
+  line2!: string;
+  recipient!: string;
+  publicKey!: BufferObject;
 }
 
-export interface Courier {
-  hashId: string;
-  name: string;
-  company: string;
-  telephone: string;
-  publicKey: BufferObject;
+export interface IAddress extends Address {}
+
+export class Courier {
+  hashId!: string;
+  name!: string;
+  company!: string;
+  telephone!: string;
+  publicKey!: BufferObject;
 }
 
-export interface Good {
-  uuid: string;
-  name: string;
-  barcode: string;
+export interface ICourier extends Courier {}
+
+export class Good {
+  uuid!: string;
+  name!: string;
+  barcode!: string;
 }
 
-export interface Stop {
-  address: string; // Hash ID
-  arrivalDeltaTimestamp: number;
-  input: { [goodUuid: string]: number };
-  output: { [goodUuid: string]: number };
-  next: Transport | undefined;
+export interface IGood extends Good {}
+
+export class Stop {
+  address!: string; // Hash ID
+  arrivalDeltaTimestamp!: number;
+  input!: { [goodUuid: string]: number };
+  output!: { [goodUuid: string]: number };
+  next: ITransport | undefined;
 }
 
-export interface Transport {
-  courier: string; // Hash ID
-  info: string;
-  destination: Stop;
+export interface IStop extends Stop {}
+
+export class Transport {
+  courier!: string; // Hash ID
+  info!: string;
+  destination!: IStop;
 }
 
-export interface CommitDetail {
-  delta: { [goodUuid: string]: number };
-  info: string;
-  timestamp: number;
+export interface ITransport extends Transport {}
+
+export class CommitDetail {
+  delta!: { [goodUuid: string]: number };
+  info!: string;
+  timestamp!: number;
 }
 
-export interface Commit {
-  detail: CommitDetail;
-  signature: BufferObject;
+export interface ICommitDetail extends CommitDetail {}
+
+export class Commit {
+  detail!: ICommitDetail;
+  signature!: BufferObject;
 }
+
+export interface ICommit extends Commit {}
 
 /**
  * Represents a point to point segment, a part of a route.
  */
-export interface Segment {
-  srcOutgoing: Commit | undefined;
-  courierReceiving: Commit | undefined;
-  courierDelivering: Commit | undefined;
-  dstIncoming: Commit | undefined;
+export class Segment {
+  srcOutgoing: ICommit | undefined;
+  courierReceiving: ICommit | undefined;
+  courierDelivering: ICommit | undefined;
+  dstIncoming: ICommit | undefined;
 }
 
-export interface Route {
-  uuid: string;
-  goods: { [goodUuid: string]: number };
-  addresses: { [addressHashId: string]: Address };
-  couriers: { [courierHashId: string]: Courier };
-  source: Stop;
-  commits: Segment[];
+export interface ISegment extends Segment {}
+
+export class Route {
+  uuid!: string;
+  goods!: { [goodUuid: string]: number };
+  addresses!: { [addressHashId: string]: IAddress };
+  couriers!: { [courierHashId: string]: ICourier };
+  source!: IStop;
+  commits!: ISegment[];
 }
 
-export interface RouteProposal {
-  route: Route,
-  signatures: { [partyHashId: string]: BufferObject };
+export interface IRoute extends Route {}
+
+export class RouteProposal {
+  route!: IRoute;
+  signatures!: { [partyHashId: string]: BufferObject };
 }
+
+export interface IRouteProposal extends RouteProposal {}
