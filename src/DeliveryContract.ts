8c9e51e5d0544b8e1c43b8448983915baf3818b9
Jerrylum <serverofjerry@gmail.com>
2024-03-09 16:32:16 +0800

    Work in progress
    Co-authored-by: Fakeye <crapper@users.noreply.github.com>

--- src/DeliveryContract.ts
+++ src/DeliveryContract.ts
@@ -4,32 +4,47 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-import { IRouteProposal } from "Models";
+
+
 import { Context, Contract, Info, Returns, Transaction } from "fabric-contract-api";
 import { Buffer } from "buffer";
 import { generateKeyPairSync, createSign, createVerify, createHash } from "node:crypto";
+import { IRouteProposal, IAddress, ICourier } from "./Models";
+
+// state standards
+// ctx.stub.putState("rp-uuid", Buffer.from("{ }")); // route proposal
+// ctx.stub.putState("ad-hashId", Buffer.from("{ }")); // address
+// ctx.stub.putState("cr-hashId", Buffer.from("{ }")); // courier
+// ctx.stub.putState("gs-uuid", Buffer.from("{ }")); // goods
+// ctx.stub.getStateByRange("rp-", "rp-"); // get all route proposals
 
 @Info({ title: "DeliveryChain", description: "Smart Contract for handling delivery chain." })
 export class DeliveryContract extends Contract {
   @Transaction(false)
   @Returns("boolean")
   public async routeExists(ctx: Context, routeUuid: string): Promise<boolean> {
-    // ctx.stub.putState("proposals", Buffer.from("{ \"uuid1\": {}, ... }"));
-    // ctx.stub.putState("proposals", Buffer.from("[ \"uuid1\", ... ]"));
-    // ctx.stub.putState("routes", Buffer.from("[ \"uuid1\", ... ]"));
-    // ctx.stub.putState("addressBook", Buffer.from("{ }"));
-    // ctx.stub.putState("courierBook", Buffer.from("{ }"));
-    // ctx.stub.putState("ad-hashId1", Buffer.from("{ }"));
-    // ctx.stub.putState("pp-uuid1", Buffer.from("{ }"));
-    // ctx.stub.putState("rt-uuid1", Buffer.from("{ }"));
+    const data = await ctx.stub.getState("rp-"+routeUuid);
+    return !!data && data.length > 0;
+  }
 
-    ctx.stub.putState("pp-uuid1", Buffer.from("{ route: {...}, sign: {} }"));
-    ctx.stub.putState("pp-uuid1", Buffer.from("{ route: {...}, sign: {a: ...} }"));
-    ctx.stub.putState("pp-uuid1", Buffer.from("{ route: {...}, sign: {a: ..., b: ...} }"));
+  @Transaction(false)
+  @Returns("boolean")
+  public async addressExists(ctx: Context, hashId: string): Promise<boolean> {
+    const data = await ctx.stub.getState("ad-"+hashId);
+    return !!data && data.length > 0;
+  }
 
-    ctx.stub.getStateByRange("pp-", "pp-");
+  @Transaction(false)
+  @Returns("boolean")
+  public async courierExists(ctx: Context, hashId: string): Promise<boolean> {
+    const data = await ctx.stub.getState("cr-"+hashId);
+    return !!data && data.length > 0;
+  }
 
-    const data = await ctx.stub.getState(routeUuid);
+  @Transaction(false)
+  @Returns("boolean")
+  public async goodsExists(ctx: Context, goodsUuid: string): Promise<boolean> {
+    const data = await ctx.stub.getState("gs-"+goodsUuid);
     return !!data && data.length > 0;
   }
 
@@ -49,80 +64,32 @@ export class DeliveryContract extends Contract {
       throw new Error(`The courier hash is not valid.`);
     }
     const buffer = Buffer.from(JSON.stringify(routeProposal));
-    await ctx.stub.putState(routeProposal.route.uuid, buffer);
+    await ctx.stub.putState("rp-"+routeProposal.route.uuid, buffer);
   }
   
   @Transaction()
-  public async signRouteProposal(ctx: Context, routeId: string, address: string, sign: string): Promise<void> {
+  public async signRouteProposal(ctx: Context, routeId: string, unsignedData: string, sign: string): Promise<void> {
+    // unsignedData is the data to be signed for the local user, for courier, it is the Json stringified ICourier(without hashID), for address, it is the Json stringified IAddress(without hashID)
     const routeProposal = await this.readRouteProposal(ctx, routeId);
     // for each key in routeProposal.signatures
     const addressBook = routeProposal.route.addresses;
     const courierBook = routeProposal.route.couriers;
-    var pk = undefined;
-    for(const key in Object.keys(routeProposal.signatures)) {
-      if (Object.keys(addressBook).includes(key)) {
-        pk = addressBook[key].publicKey.data;
-      }else if (Object.keys(courierBook).includes(key)) {
-        pk = courierBook[key].publicKey.data;
-      }
-      // verify the signature TODO
+    if (!await this.validateAddressCourierHash(routeProposal.route.addresses)) {
+      throw new Error(`The address hash is not valid.`);
     }
+    if (!await this.validateAddressCourierHash(routeProposal.route.couriers)) {
+      throw new Error(`The courier hash is not valid.`);
+    }
+    if (!await this.validateRouteProposalSignatures(routeProposal)) {
+      throw new Error(`The route proposal signature is not valid.`);
+    }
+    routeProposal.signatures[await this.objectToSHA256(unsignedData)] = sign;
+    const buffer = Buffer.from(JSON.stringify(routeProposal));
+    await ctx.stub.putState("rp-"+routeId, buffer);
   }
 
-  // @Transaction()
-  // public async shipProductTo(ctx: Context, productId: string, newLocation: string, arrivalDate: string): Promise<void> {
-  //     const exists: boolean = await this.productExists(ctx, productId);
-  //     if (!exists) {
-  //         throw new Error(`The product ${productId} does not exist.`);
-  //     }
-
-  //     this.requireField(newLocation, 'newLocation');
-  //     this.requireField(arrivalDate, 'arrivalDate');
-
-  //     const product = await this.readProduct(ctx, productId);
-
-  //     product.locationData.previous.push(new ProductLocationEntry({
-  //         arrivalDate: product.locationData.current.arrivalDate,
-  //         location: product.locationData.current.location
-  //     }));
-  //     product.locationData.current.arrivalDate = arrivalDate;
-  //     product.locationData.current.location = newLocation;
-
-  //     const buffer = Buffer.from(JSON.stringify(product));
-  //     await ctx.stub.putState(productId, buffer);
-  // }
-
-  // @Transaction(false)
-  // @Returns('Product')
-  // public async getProduct(ctx: Context, productId: string): Promise<Product> {
-  //     const exists: boolean = await this.productExists(ctx, productId);
-  //     if (!exists) {
-  //         throw new Error(`The product ${productId} does not exist.`);
-  //     }
-
-  //     return this.readProduct(ctx, productId);
-  // }
-
-  // @Transaction(false)
-  // @Returns('ProductHistory')
-  // public async getProductWithHistory(ctx: Context, productId: string): Promise<ProductWithHistory> {
-  //     const exists: boolean = await this.productExists(ctx, productId);
-  //     if (!exists) {
-  //         throw new Error(`The product ${productId} does not exist.`);
-  //     }
-
-  //     const product = await this.readProduct(ctx, productId);
-  //     const productWithHistory = new ProductWithHistory(product);
-  //     productWithHistory.componentProducts = [];
-
-  //     for (const childProductId of product.componentProductIds) {
-  //         const childProduct = await this.readProduct(ctx, childProductId);
-  //         productWithHistory.componentProducts.push(childProduct);
-  //     }
-
-  //     return productWithHistory;
-  // }
-
+  @Transaction(false)
+  @Returns("IRouteProposal")
   private async readRouteProposal(ctx: Context, routeId: string): Promise<IRouteProposal> {
     const data = await ctx.stub.getState(routeId);
     if (!data || data.length === 0) {
@@ -133,6 +100,122 @@ export class DeliveryContract extends Contract {
     return routeProposal;
   }
 
+  // 2 functions for create a new address, update an existing address
+  @Transaction()
+  public async createAddress(ctx: Context, addressJsonStr: string): Promise<void> {
+    const address = JSON.parse(addressJsonStr) as IAddress;
+    // check the address value on "hashId", "line1", "line2", "recipient", "publicKey"
+    // if the value is not valid, throw an error
+    if (address.hashId === undefined || address.line1 === undefined || address.line2 === undefined || address.recipient === undefined || address.publicKey === undefined) {
+      throw new Error(`The address ${address.hashId} is not valid.`);
+    }
+    // check the address hash
+    if (address.hashId !== await this.objectToSHA256(address)) {
+      throw new Error(`The address ${address.hashId} is not valid.`);
+    }
+    const exists: boolean = await this.addressExists(ctx, address.hashId);
+    if (exists) {
+        throw new Error(`The address ${address.hashId} already exists.`);
+    }
+    const buffer = Buffer.from(JSON.stringify(address));
+    await ctx.stub.putState("ad-"+address.hashId, buffer);
+  }
+  
+  @Transaction()
+  public async updateAddress(ctx: Context, addressJsonStr: string): Promise<void> {
+    const address = JSON.parse(addressJsonStr) as IAddress;
+    // check the address value on "hashId", "line1", "line2", "recipient", "publicKey"
+    // if the value is not valid, throw an error
+    if (address.hashId === undefined || address.line1 === undefined || address.line2 === undefined || address.recipient === undefined || address.publicKey === undefined) {
+      throw new Error(`The address ${address.hashId} is not valid.`);
+    }
+    // check the address hash
+    if (address.hashId !== await this.objectToSHA256(address)) {
+      throw new Error(`The address ${address.hashId} is not valid.`);
+    }
+    const exists: boolean = await this.addressExists(ctx, address.hashId);
+    if (!exists) {
+        throw new Error(`The address ${address.hashId} does not exist.`);
+    }
+    const buffer = Buffer.from(JSON.stringify(address));
+    await ctx.stub.putState("ad-"+address.hashId, buffer);
+  }
+
+  // 2 functions for create a new courier, update an existing courier
+  @Transaction()
+  public async createCourier(ctx: Context, courierJsonStr: string): Promise<void> {
+    const courier = JSON.parse(courierJsonStr) as ICourier;
+    // check the courier value on "hashId", "name", "company", "telephone", "publicKey"
+    // if the value is not valid, throw an error
+    if (courier.hashId === undefined || courier.name === undefined || courier.company === undefined || courier.telephone === undefined || courier.publicKey === undefined) {
+      throw new Error(`The courier ${courier.hashId} is not valid.`);
+    }
+    // check the courier hash
+    if (courier.hashId !== await this.objectToSHA256(courier)) {
+      throw new Error(`The courier ${courier.hashId} is not valid.`);
+    }
+    const exists: boolean = await this.courierExists(ctx, courier.hashId);
+    if (exists) {
+        throw new Error(`The courier ${courier.hashId} already exists.`);
+    }
+    const buffer = Buffer.from(JSON.stringify(courier));
+    await ctx.stub.putState("cr-"+courier.hashId, buffer);
+  }
+
+  @Transaction()
+  public async updateCourier(ctx: Context, courierJsonStr: string): Promise<void> {
+    const courier = JSON.parse(courierJsonStr) as ICourier;
+    // check the courier value on "hashId", "name", "company", "telephone", "publicKey"
+    // if the value is not valid, throw an error
+    if (courier.hashId === undefined || courier.name === undefined || courier.company === undefined || courier.telephone === undefined || courier.publicKey === undefined) {
+      throw new Error(`The courier ${courier.hashId} is not valid.`);
+    }
+    // check the courier hash
+    if (courier.hashId !== await this.objectToSHA256(courier)) {
+      throw new Error(`The courier ${courier.hashId} is not valid.`);
+    }
+    const exists: boolean = await this.courierExists(ctx, courier.hashId);
+    if (!exists) {
+        throw new Error(`The courier ${courier.hashId} does not exist.`);
+    }
+    const buffer = Buffer.from(JSON.stringify(courier));
+    await ctx.stub.putState("cr-"+courier.hashId, buffer);
+  }
+
+  // 2 functions for create a new goods, update an existing goods
+  @Transaction()
+  public async createGoods(ctx: Context, goodsJsonStr: string): Promise<void> {
+    const goods = JSON.parse(goodsJsonStr) as any;
+    // check the goods value on "uuid", "name", "barcode"
+    // if the value is not valid, throw an error
+    if (goods.uuid === undefined || goods.name === undefined || goods.barcode === undefined) {
+      throw new Error(`The goods ${goods.uuid} is not valid.`);
+    }
+    const exists: boolean = await this.goodsExists(ctx, goods.uuid);
+    if (exists) {
+        throw new Error(`The goods ${goods.uuid} already exists.`);
+    }
+    const buffer = Buffer.from(JSON.stringify(goods));
+    await ctx.stub.putState("gs-"+goods.uuid, buffer);
+  }
+
+  @Transaction()
+  public async updateGoods(ctx: Context, goodsJsonStr: string): Promise<void> {
+    const goods = JSON.parse(goodsJsonStr) as any;
+    // check the goods value on "uuid", "name", "barcode"
+    // if the value is not valid, throw an error
+    if (goods.uuid === undefined || goods.name === undefined || goods.barcode === undefined) {
+      throw new Error(`The goods ${goods.uuid} is not valid.`);
+    }
+    const exists: boolean = await this.goodsExists(ctx, goods.uuid);
+    if (!exists) {
+        throw new Error(`The goods ${goods.uuid} does not exist.`);
+    }
+    const buffer = Buffer.from(JSON.stringify(goods));
+    await ctx.stub.putState("gs-"+goods.uuid, buffer);
+  }
+
+  @Transaction(false)
   private async objectToSHA256(obj: any): Promise<string> {
     const hash = createHash('sha256');
     hash.update(JSON.stringify(obj));
@@ -156,7 +239,35 @@ export class DeliveryContract extends Contract {
     }
     return true;
   }
+
+  @Transaction(false)
+  @Returns("boolean")
+  private async validateRouteProposalSignatures(routeProposal: IRouteProposal): Promise<boolean> {
+    var pk = undefined;
+    const addressBook = routeProposal.route.addresses;
+    const courierBook = routeProposal.route.couriers;
+    for(const key in Object.keys(routeProposal.signatures)) {
+      if (Object.keys(addressBook).includes(key)) {
+        pk = addressBook[key].publicKey;
+      }else if (Object.keys(courierBook).includes(key)) {
+        pk = courierBook[key].publicKey;
+      }
+      // irrelevant signature exists
+      if (pk === undefined) { 
+        continue;
+      }
+      const data = JSON.stringify(routeProposal.route);
+      const signedData = routeProposal.signatures[key];
+      if (!await this.verifyData(data, signedData, pk)) {
+        return false;
+      }
+      pk = undefined;
+    }
+    return true;
+  }
   
+  @Transaction(false)
+  @Returns("string")
   private async signData(data: string, privateKey: string): Promise<string> {
     const sign = createSign('SHA256');
     sign.update(data);
@@ -164,6 +275,8 @@ export class DeliveryContract extends Contract {
     return sign.sign(privateKey, 'hex');
   }
 
+  @Transaction(false)
+  @Returns("boolean")
   private async verifyData(data: string, signature: string, publicKey: string): Promise<boolean> {
       const verify = createVerify('SHA256');
       verify.update(data);
@@ -171,15 +284,9 @@ export class DeliveryContract extends Contract {
       return verify.verify(publicKey, signature, 'hex');
   }
 
+  @Transaction(false)
   private omitProperty<T, K extends keyof any>(obj: T, keyToOmit: K): Omit<T, K> {
     const { [keyToOmit]: _, ...rest } = obj;
     return rest as Omit<T, K>;
   }
-
-
-  // private requireField(value: string | number, fieldName: string) {
-  //     if (!value) {
-  //         throw new Error(`The '${fieldName}' field is required.`);
-  //     }
-  // }
 }
--- src/Models.spec.ts
+++ src/Models.spec.ts
@@ -1,12 +1,13 @@
 import { Context, Contract, Transaction } from "fabric-contract-api";
 
-import { ChaincodeFromContract } from "./types/fabric-shim-internal";
+import { ChaincodeFromContract } from "./lib/fabric-shim-internal";
 import { ChaincodeStub } from "fabric-shim";
 import sinon = require("sinon");
 import { expect } from "chai";
-import { IAddress, Address } from "./Models";
+import { IAddress, Address, ICourier, Courier, IGood, Good } from "./Models";
 import { serializers } from ".";
 import { SimpleJSONSerializer } from "./SimpleJSONSerializer";
+import { BetterTransaction } from "./annotations/BetterTransaction";
 
 abstract class ExtendedChaincodeStub extends ChaincodeStub {
   abstract getBufferArgs(): Buffer[];
@@ -43,14 +44,24 @@ describe("Test Models Serialization", async () => {
   const metadata = require(`${rootPath}/META-INF/metadata.json`);
 
   class TextContract extends Contract {
-    @Transaction()
+    @BetterTransaction()
     public async textTransaction(ctx: Context, name: string, age: number): Promise<string> {
       return "hello " + name + " " + age;
     }
 
-    @Transaction()
+    @BetterTransaction()
     public async sendAddress(ctx: Context, addr: Address): Promise<Address> {
       return addr;
+    }
+
+    @BetterTransaction()
+    public async sendCourier(ctx: Context, courier: Courier): Promise<Courier> {
+      return courier;
+    }
+
+    @BetterTransaction()
+    public async sendGood(ctx: Context, good: Good): Promise<Good> {
+      return good;
     }
   }
 
@@ -106,16 +117,91 @@ describe("Test Models Serialization", async () => {
       recipient: "recipient",
       publicKey: Buffer.from("publicKey").toString("hex")
     };
-    const expected = Buffer.from(JSON.stringify(addr));
+    const expected = SimpleJSONSerializer.serialize(addr);
 
     const stub = createStub("sendAddress", addr);
     const actual = await c.Invoke(stub);
     expect(actual.payload).to.deep.equal(expected);
 
-    const addr2: IAddress = SimpleJSONSerializer.deserialize(actual.payload);   
+    const addr2: IAddress = SimpleJSONSerializer.deserialize(actual.payload);
 
     const stub2 = createStub("sendAddress", addr2);
     const actual2 = await c.Invoke(stub2);
     expect(actual2.payload).to.deep.equal(expected);
   });
+
+  it("invoke transaction with incorrect correct Address model should return error", async () => {
+    const addr: IAddress = {} as any;
+
+    const stub = createStub("sendAddress", addr);
+    const actual = await c.Invoke(stub);
+
+    expect(actual.payload).to.be.undefined;
+    expect(actual.message).to.equal(
+      `Unable to validate parameter due to ["should have required property 'hashId'","should have required property 'line1'","should have required property 'line2'","should have required property 'recipient'","should have required property 'publicKey'"]`
+    );
+  });
+
+  it("invoke transaction with correct Courier model should return correct result", async () => {
+    const courier: ICourier = {
+      hashId: "hashId",
+      name: "name",
+      company: "company",
+      telephone: "telephone",
+      publicKey: Buffer.from("publicKey").toString("hex")
+    };
+    const expected = SimpleJSONSerializer.serialize(courier);
+
+    const stub = createStub("sendCourier", courier);
+    const actual = await c.Invoke(stub);
+    expect(actual.payload).to.deep.equal(expected);
+
+    const courier2: ICourier = SimpleJSONSerializer.deserialize(actual.payload);
+
+    const stub2 = createStub("sendCourier", courier2);
+    const actual2 = await c.Invoke(stub2);
+    expect(actual2.payload).to.deep.equal(expected);
+  });
+
+  it("invoke transaction with incorrect correct Courier model should return error", async () => {
+    const courier: ICourier = {} as any;
+
+    const stub = createStub("sendCourier", courier);
+    const actual = await c.Invoke(stub);
+
+    expect(actual.payload).to.be.undefined;
+    expect(actual.message).to.equal(
+      `Unable to validate parameter due to ["should have required property 'hashId'","should have required property 'name'","should have required property 'company'","should have required property 'telephone'","should have required property 'publicKey'"]`
+    );
+  });
+
+  it("invoke transaction with correct Good model should return correct result", async () => {
+    const good: IGood = {
+      uuid: "uuid",
+      name: "name",
+      barcode: "barcode"
+    };
+    const expected = SimpleJSONSerializer.serialize(good);
+
+    const stub = createStub("sendGood", good);
+    const actual = await c.Invoke(stub);
+    expect(actual.payload).to.deep.equal(expected);
+
+    const good2: IGood = SimpleJSONSerializer.deserialize(actual.payload);
+
+    const stub2 = createStub("sendGood", good2);
+    const actual2 = await c.Invoke(stub2);
+    expect(actual2.payload).to.deep.equal(expected);
+  });
+
+  it("invoke transaction with incorrect correct Good model should return error", async () => {
+    const good: IGood = {} as any;
+
+    const stub = createStub("sendGood", good);
+    const actual = await c.Invoke(stub);
+
+    expect(actual.payload).to.be.undefined;
+    expect(actual.message).to.equal(`Unable to validate parameter due to ["should have required property 'uuid'","should have required property 'name'","should have required property 'barcode'"]`);
+  });
+  
 });
