2beb722b195dc78916727ea12f7c2912de1ba5f9
Jerrylum <serverofjerry@gmail.com>
2024-03-16 15:05:35 +0800

    Export DeliveryContract

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
+import { Contract } from "fabric-contract-api";
 import { SimpleJSONSerializer } from "./SimpleJSONSerializer";
 import { Serializers } from "./lib/fabric-shim-internal";
+import { DeliveryContract } from "./DeliveryContract";
 
 export const serializers = {
   transaction: "SimpleJSONSerializer", // Set default serializer for transactions
@@ -7,3 +9,5 @@ export const serializers = {
     SimpleJSONSerializer: SimpleJSONSerializer
   }
 } satisfies Serializers;
+
+export const contracts: typeof Contract[] = [DeliveryContract];
